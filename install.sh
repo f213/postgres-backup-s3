7fee7cee4f47ce93efe57362229d5de133e0f4f9
Fedor Borshev <fedor@borshev.com>
2021-02-05 14:29:03 +0300

    Hi there :neckbeard:

---
+++ install.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+# exit if a command fails
+set -e
+
+
+apk update
+
+# install pg_dump
+apk add postgresql-client
+
+# install s3 tools
+apk --no-cache add aws-cli bash findutils groff less python3 tini inotify-tools
+
+# install go-cron
+apk add curl
+curl -L --insecure https://github.com/odise/go-cron/releases/download/v0.0.6/go-cron-linux.gz | zcat > /usr/local/bin/go-cron
+chmod u+x /usr/local/bin/go-cron
+apk del curl
+
+
+# cleanup
+rm -rf /var/cache/apk/*
