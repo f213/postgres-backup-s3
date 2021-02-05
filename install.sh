30f2b6f391e3f2c4917fe1f2dfce6f3c536dd761
Fedor Borshev <fedor@borshev.com>
2021-02-05 15:25:34 +0300

    odise/go-cron version dump

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ apk --no-cache add aws-cli bash findutils groff less python3 tini inotify-tools
 
 # install go-cron
 apk add curl
-curl -L --insecure https://github.com/odise/go-cron/releases/download/v0.0.6/go-cron-linux.gz | zcat > /usr/local/bin/go-cron
+curl -L https://github.com/odise/go-cron/releases/download/v0.0.7/go-cron-linux.gz | zcat > /usr/local/bin/go-cron
 chmod u+x /usr/local/bin/go-cron
 apk del curl
 
